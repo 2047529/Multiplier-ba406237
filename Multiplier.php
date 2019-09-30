7b611bff96b571595c7cf1c36478d04f3448d5ca
Arjen Bos <2047529@talnet.nl>
2019-09-30 17:01:28 +0200

    Uitwerking van de exercise

---
+++ Multiplier.php
@@ -0,0 +1,17 @@
+<?php
+
+$zin1 = "Voer een getal in:\n";
+echo $zin1;
+$first_number = readline();
+$zin2 = "Voer een tweede getal in:\n";
+echo $zin2;
+$second_number = readline();
+$total = $first_number * $second_number;
+echo $total;
+
+
+
+
+
+
+?>
